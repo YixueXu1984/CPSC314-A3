fa015727bb7f19159559e639b20829ad32ecc4bc
yixue <v7e0b@ugrad.cs.ubc.ca>
2019-02-26 18:21:32 -0800

    fog completed

--- glsl/fog.fs.glsl
+++ glsl/fog.fs.glsl
@@ -1,10 +1,47 @@
 #version 300 es
 
+uniform float fogDensity;
+uniform vec3 spotlightPosition;
+uniform vec3 lightDirectionUniform;
+
+uniform float kSpecularUniform;
+uniform float shininessUniform;
+uniform float kAmbientUniform;
+uniform float kDiffuseUniform;
+
+in vec3 nb;
+in vec3 vb;
+in float distToCam;
+
+
 out vec4 out_FragColor;
 
 void main() {
 
 	// TODO: PART 1C
 
-	out_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+		vec3 lightFogColor = vec3(0.5, 0.5, 0.5);
+
+			vec3 nbn = normalize(nb);
+        	vec3 lb = normalize(vec3(viewMatrix * vec4(lightDirectionUniform, 0.0)));
+        	vec3 vbv = normalize(-vb);
+
+    	//AMBIENT
+    		vec3 ambientColor = vec3(0.4, 0.4, 0.4);
+        	vec3 lightColor = vec3(1.0, 1.0, 1.0);
+        	vec3 light_AMB = ambientColor * kAmbientUniform;
+
+    	//DIFFUSE
+    	float diffuse = max(0.0, dot(nbn,lb));
+        vec3 dIntensity = lightColor * diffuse;
+        vec3 light_DFF = dIntensity * kDiffuseUniform;
+
+        float fogLevel = 1.0/(exp(distToCam * fogDensity));
+
+
+
+	vec3 TOTAL = light_AMB + light_DFF ;
+	out_FragColor = mix(vec4(TOTAL, 1.0), vec4(lightFogColor, 1.0), 1.0 - fogLevel);
+
+	//out_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
 }
--- glsl/fog.vs.glsl
+++ glsl/fog.vs.glsl
@@ -1,8 +1,17 @@
 #version 300 es
 
+out vec3 nb;
+out vec3 vb;
+
+out float distToCam;
+
 void main() {
 
 	// TODO: PART 1C
+		nb = normalMatrix * normal;
+        vb = vec3(modelViewMatrix * vec4(position, 1.0));
+
+        distToCam = vec4(projectionMatrix * viewMatrix * modelMatrix * vec4(position,1.0)).w;
 
     gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position,1.0);
 }
